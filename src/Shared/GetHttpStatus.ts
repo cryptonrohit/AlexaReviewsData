31c64f6a9317d8506e472e2176e78fb6678bf261
z003w4hv <rohit.pal@siemens.com>
2022-01-11 22:31:40 +0530

    feat: adapted httpstatus file to include more operations

--- src/Shared/GetHttpStatus.ts
+++ src/Shared/GetHttpStatus.ts
@@ -2,6 +2,10 @@ import { Operation } from "../Model/Operation";
 
 export function getHttpStatusData(operation: Operation) {
     switch (operation) {
+        case Operation.Success:
+            return {statusCode: 200, outputData: "All good."};
+        case Operation.NoDataFound:
+            return {statusCode: 404, outputData: "No data available"};
         case Operation.Created:
             return {statusCode: 201, outputData: "Created"};
         case Operation.Error:
