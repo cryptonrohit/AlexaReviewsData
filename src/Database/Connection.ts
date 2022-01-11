7099a201a75bfa3a9b1ebbe2439118b863e53af0
z003w4hv <rohit.pal@siemens.com>
2022-01-12 03:19:04 +0530

    feat: added connection string

--- src/Database/Connection.ts
+++ src/Database/Connection.ts
@@ -6,11 +6,7 @@ class DBConnection {
         const configData = {
             client: "pg",
             connection: {
-                host: "ec2-174-129-37-144.compute-1.amazonaws.com",
-                user: "fozhrhaekupvez",
-                password: "db88d2b110e6f9dc4a40da5f522e0c054a05d27885a2ce5e1d76cf60eb50d837",
-                database: "d10ntjiount1qd",
-                port: 5432,
+                connectionString: process.env.DATABASE_URL,
                 ssl: { 
                     rejectUnauthorized: false 
                 }
