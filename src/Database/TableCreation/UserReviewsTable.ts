4cc83b2a0fca15d87751f01363ee98da422cba07
z003w4hv <rohit.pal@siemens.com>
2022-01-11 18:47:59 +0530

    feat: added schema for userReviewsTable

--- src/Database/TableCreation/UserReviewsTable.ts
+++ src/Database/TableCreation/UserReviewsTable.ts
@@ -4,6 +4,15 @@ import { USER_REVIEWS_DATA } from "../TableNames/TableNames";
 class UserReviewsTable {
     async create(dbConnector: Knex) {
         console.log(`[DB] Creating new table: ${USER_REVIEWS_DATA}`);
+        return await dbConnector.schema.createTable(USER_REVIEWS_DATA , (table) => {
+            table.text("review");
+            table.string("author");
+            table.string("review_source");
+            table.integer("rating");
+            table.string("title");
+            table.string("product_name");
+            table.timestamp("reviewed_date");
+        })
     }
 }
 const userReviewsTable = new UserReviewsTable();
