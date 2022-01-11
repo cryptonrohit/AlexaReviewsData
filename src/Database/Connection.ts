5cfbf3fb6de6f80a4348b344f9bedba69be75d56
z003w4hv <rohit.pal@siemens.com>
2022-01-12 02:57:11 +0530

    feat: added entire connection object

--- src/Database/Connection.ts
+++ src/Database/Connection.ts
@@ -5,9 +5,15 @@ class DBConnection {
     connectionToDB() {
         const configData = {
             client: "pg",
-            connection: process.env.DATABASE_URL,
-            ssl: { 
-                rejectUnauthorized: false 
+            connection: {
+                host: "ec2-174-129-37-144.compute-1.amazonaws.com",
+                user: "fozhrhaekupvez",
+                password: "db88d2b110e6f9dc4a40da5f522e0c054a05d27885a2ce5e1d76cf60eb50d837",
+                database: "d10ntjiount1qd",
+                port: 5432,
+                ssl: { 
+                    rejectUnauthorized: false 
+                }
             }
         }
         return Knex(configData);
