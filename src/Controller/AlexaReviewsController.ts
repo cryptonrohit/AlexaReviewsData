fbc3f494abd11dd1ed60a5f00c208b77956e98f8
z003w4hv <rohit.pal@siemens.com>
2022-01-11 22:30:14 +0530

    feat: adapted controller to get all reviews

--- src/Controller/AlexaReviewsController.ts
+++ src/Controller/AlexaReviewsController.ts
@@ -1,11 +1,19 @@
 import { Request, Response } from "express";
-import alexaReviewsService from "../Services/AlexaReviewsService";
+import getAlexaReviewsService from "../Services/GetAlexaReviewsService";
+import insertAlexaReviewsService from "../Services/InsertAlexaReviewsService";
 
 class AlexaReviewsController {
     async insertReviewsData(req: Request, res: Response) {
         const uploadedData = req['files'].reviewsFile.data.toString();
-        const result = await alexaReviewsService.execute(uploadedData);
+        const result = await insertAlexaReviewsService.execute(uploadedData);
         res.status(result.statusCode).send(result.outputData);
+    }
+    async getReviewsData(req: Request, res: Response) {
+        const storeType = req.query.storeType as string;
+        const rating = req.query.rating as unknown as number;
+        const date = req.query.date as unknown as Date;
+        const result = await getAlexaReviewsService.execute({ storeType, rating, date });
+        res.status(result.status).send(result.data);
     }
 }
 const alexaReviewsController = new AlexaReviewsController();
