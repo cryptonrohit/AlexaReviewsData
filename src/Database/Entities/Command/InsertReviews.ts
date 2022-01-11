ec1850d52bc96126e390b167ae1c222073926cdf
z003w4hv <rohit.pal@siemens.com>
2022-01-11 18:59:11 +0530

    feat: added inserting to db code

---
+++ src/Database/Entities/Command/InsertReviews.ts
@@ -0,0 +1,21 @@
+import { IUserReviewsModel } from "../../../Model/IUserReviewsModel";
+import { Operation } from "../../../Model/Operation";
+import db from "../../Configuration";
+import { USER_REVIEWS_DATA } from "../../TableNames/TableNames";
+
+class InsertReviews {
+    async execute(userReviewsData: IUserReviewsModel[]): Promise<Operation> {
+        try {
+            await db.DBInstance().dbConnector
+                .insert(userReviewsData)
+                .into(USER_REVIEWS_DATA)
+                .then(response => response);
+            return Operation.Created;
+        } catch (error) {
+            console.error("[DB] Error inserting user Reviews Data. Error: ", error);
+            return Operation.Error;
+        }
+    }
+}
+const insertReviews = new InsertReviews();
+export default insertReviews;
---
+++ src/Model/IUserReviewsModel.ts
@@ -0,0 +1,9 @@
+export interface IUserReviewsModel {
+    review: string,
+    author: string;
+    review_source: string;
+    rating: number;
+    title: string;
+    product_name: string;
+    reviewed_date: string;
+}
