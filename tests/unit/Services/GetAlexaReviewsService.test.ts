601ce14b433733d51de010231e6085197c82be4e
z003w4hv <rohit.pal@siemens.com>
2022-01-12 20:52:08 +0530

    feat: test for file GetAlexaReviewsService added

---
+++ tests/unit/Services/GetAlexaReviewsService.test.ts
@@ -0,0 +1,79 @@
+import sinon, { SinonStub } from "sinon";
+import { expect } from "chai";
+import getAllReviews from "../../../src/Database/Entities/Query/GetAllReviews";
+import { IGetUserReviewsModel } from "../../../src/Model/IGetUserReviewsModel";
+import { Operation } from "../../../src/Model/Operation";
+import getAlexaReviewsService from "../../../src/Services/GetAlexaReviewsService";
+
+describe("GetAlexaReviewsService tests", () => {
+    let getAllReviewsStub: SinonStub;
+    const filterTestData: IGetUserReviewsModel  = {
+        storeType: "iTunes",
+        rating: 4,
+        date: "2017-12-30T00:00:00.000Z" as unknown as Date
+    }
+    const reviewsTestData = [
+        {
+            "review": "Pero deberia de poder cambiarle el idioma a alexa",
+            "author": "WarcryxD",
+            "review_source": "iTunes",
+            "rating": 4,
+            "title": "Excelente",
+            "product_name": "Amazon Alexa",
+            "reviewed_date": "2018-01-12T02:27:03.000Z"
+        },
+        {
+            "review": "Cannot fix connection glitches without this-also fix connection glitches \n\nSmart things sees my light and Alexa doesn’t :(\n\n*update new devices “unresponsive” each day...they are working fine in SmartThings. No way to delete disabled devices. Very poor.",
+            "author": "Ranchorat",
+            "review_source": "iTunes",
+            "rating": 1,
+            "title": "Need to be able to delete devices",
+            "product_name": "Amazon Alexa",
+            "reviewed_date": "2017-12-06T13:06:33.000Z"
+        }
+    ]
+
+    beforeEach(() => {
+        getAllReviewsStub = sinon.stub(getAllReviews, "get");
+    })
+
+    afterEach(() => {
+        getAllReviewsStub.restore();
+    });
+
+    it("execute_WhenDataInDB_ShouldReturnStatusCode200", async () => {
+        // Arrange
+        getAllReviewsStub.returns({ data: reviewsTestData, totalCount: reviewsTestData.length, status: Operation.Success });
+        const expectedResult = {data: reviewsTestData, totalCount: reviewsTestData.length, status: 200};
+
+        // Act
+        const actualResult = await getAlexaReviewsService.execute(filterTestData);
+    
+        // Assert
+        expect(actualResult).to.eql(expectedResult);
+    });
+
+    it("execute_WhenNoDataInDB_ShouldReturnStatusCode404", async () => {
+        // Arrange
+        getAllReviewsStub.returns({ data: [], totalCount: 0, status: Operation.NoDataFound });
+        const expectedResult = {data: [], totalCount: 0, status: 404};
+
+        // Act
+        const actualResult = await getAlexaReviewsService.execute(filterTestData);
+    
+        // Assert
+        expect(actualResult).to.eql(expectedResult);
+    });
+
+    it("execute_WhenDBError_ShouldReturnStatusCode500", async () => {
+        // Arrange
+        getAllReviewsStub.returns({ status: Operation.Error });
+        const expectedResult = {data: undefined, totalCount: undefined, status: 500};
+
+        // Act
+        const actualResult = await getAlexaReviewsService.execute(filterTestData);
+    
+        // Assert
+        expect(actualResult).to.eql(expectedResult);
+    });
+})
