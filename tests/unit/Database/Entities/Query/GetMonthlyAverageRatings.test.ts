4d96d67ee37c4ae9fb5a6c86adcafd7b51c69ddb
z003w4hv <rohit.pal@siemens.com>
2022-01-13 20:48:46 +0530

    feat: adapted tests

--- tests/unit/Database/Entities/Query/GetMonthlyAverageRatings.test.ts
+++ tests/unit/Database/Entities/Query/GetMonthlyAverageRatings.test.ts
@@ -8,6 +8,7 @@ describe("GetMonthlyAverageRatings tests", () => {
     let rawStub: SinonStub;
     let dbInstanceStub: SinonStub;
     let consoleStub: SinonStub;
+    const testStoreType = "iTunes";
     const monthlyAvgRatingsTestData = {
         rowCount: 2,
         rows: [
@@ -22,6 +23,24 @@ describe("GetMonthlyAverageRatings tests", () => {
                 "averagerating": "1.76",
                 "month": 1,
                 "year": 2018
+            }
+        ]
+    }
+
+    const monthlyAvgRatingsiTunesTestData = {
+        rowCount: 2,
+        rows: [
+            {
+                "review_source": "iTunes",
+                "averagerating": "1.76",
+                "month": 1,
+                "year": 2018
+            },
+            {
+                "review_source": "iTunes",
+                "averagerating": "1.71",
+                "month": 12,
+                "year": 2017
             }
         ]
     }
@@ -43,39 +62,78 @@ describe("GetMonthlyAverageRatings tests", () => {
         consoleStub.restore();
     });
 
-    it("get_WhenReviewDataInDB_ShouldReturnSuccess", async () => {
+    it("get_WhenReviewDataInDBAndStoreTypeAbsent_ShouldReturnSuccessAndAllData", async () => {
         // Arrange
         rawStub.returns(monthlyAvgRatingsTestData);
         const expectedResult = { data: monthlyAvgRatingsTestData.rows, status: Operation.Success };
 
         // Act
-        const actualResult = await getMonthlyAverageRatings.get();
+        const actualResult = await getMonthlyAverageRatings.get("");
         
         // Assert
         expect(actualResult).to.eql(expectedResult);
 
     });
 
-    it("get_WhenNoReviewDataInDB_ShouldReturnNoDataFound", async () => {
+    it("get_WhenReviewDataInDBAndStoreTypePresent_ShouldReturnSuccessAndParticularStoreTypeData", async () => {
+        // Arrange
+        rawStub.returns(monthlyAvgRatingsiTunesTestData);
+        const expectedResult = { data: monthlyAvgRatingsiTunesTestData.rows, status: Operation.Success };
+
+        // Act
+        const actualResult = await getMonthlyAverageRatings.get(testStoreType);
+        
+        // Assert
+        expect(actualResult).to.eql(expectedResult);
+
+    });
+
+    it("get_WhenNoReviewDataInDBAndStoreTypeAbsent_ShouldReturnNoDataFound", async () => {
         // Arrange
         rawStub.returns({rowCount: 0, rows: []});
         const expectedResult = { data: [], status: Operation.NoDataFound };
 
         // Act
-        const actualResult = await getMonthlyAverageRatings.get();
+        const actualResult = await getMonthlyAverageRatings.get("");
         
         // Assert
         expect(actualResult).to.eql(expectedResult);
         sinon.assert.calledWith(consoleStub, "[DB] No reviews available.");
     });
 
-    it("get_DBError_ShouldReturnError", async () => {
+    it("get_WhenNoReviewDataInDBAndStoreTypePresent_ShouldReturnNoDataFound", async () => {
+        // Arrange
+        rawStub.returns({rowCount: 0, rows: []});
+        const expectedResult = { data: [], status: Operation.NoDataFound };
+
+        // Act
+        const actualResult = await getMonthlyAverageRatings.get(testStoreType);
+        
+        // Assert
+        expect(actualResult).to.eql(expectedResult);
+        sinon.assert.calledWith(consoleStub, "[DB] No reviews available.");
+    });
+
+    it("get_DBErrorAndStoreTypeAbsent_ShouldReturnError", async () => {
         // Arrange
         rawStub.throws("test DB error.");
         const expectedResult = { status: Operation.Error };
 
         // Act
-        const actualResult = await getMonthlyAverageRatings.get();
+        const actualResult = await getMonthlyAverageRatings.get("");
+        
+        // Assert
+        expect(actualResult).to.eql(expectedResult);
+        sinon.assert.calledWith(consoleStub, "[DB] Error while fetching monthly average ratings data.");
+    });
+
+    it("get_DBErrorAndStoreTypePresent_ShouldReturnError", async () => {
+        // Arrange
+        rawStub.throws("test DB error.");
+        const expectedResult = { status: Operation.Error };
+
+        // Act
+        const actualResult = await getMonthlyAverageRatings.get(testStoreType);
         
         // Assert
         expect(actualResult).to.eql(expectedResult);
--- tests/unit/Services/GetMonthlyAverageRatingsService.test.ts
+++ tests/unit/Services/GetMonthlyAverageRatingsService.test.ts
@@ -6,6 +6,7 @@ import getMonthlyAverageRatingsService from "../../../src/Services/GetMonthlyAve
 
 describe("GetMonthlyAverageRatingsService tests", () => {
     let getMonthlyAverageRatingsStub: SinonStub;
+    const testStoreType = "iTunes";
     const monthlyAvgRatingsTestData = [
         {
             "review_source": "GooglePlayStore",
@@ -21,6 +22,21 @@ describe("GetMonthlyAverageRatingsService tests", () => {
         }
     ]
 
+    const monthlyAvgRatingsiTunesTestData = [
+        {
+            "review_source": "iTunes",
+            "averagerating": "1.76",
+            "month": 1,
+            "year": 2018
+        },
+        {
+            "review_source": "iTunes",
+            "averagerating": "1.71",
+            "month": 12,
+            "year": 2017
+        }
+    ]
+
     beforeEach(() => {
         getMonthlyAverageRatingsStub = sinon.stub(getMonthlyAverageRatings, "get");
     })
@@ -29,37 +45,73 @@ describe("GetMonthlyAverageRatingsService tests", () => {
         getMonthlyAverageRatingsStub.restore();
     });
 
-    it("execute_WhenDataInDB_ShouldReturnStatusCode200", async () => {
+    it("execute_WhenDataInDBAndStoreTypeAbsent_ShouldReturnStatusCode200AndAllData", async () => {
         // Arrange
         getMonthlyAverageRatingsStub.returns({ data: monthlyAvgRatingsTestData, status: Operation.Success });
         const expectedResult = {data: monthlyAvgRatingsTestData, status: 200};
 
         // Act
-        const actualResult = await getMonthlyAverageRatingsService.execute();
+        const actualResult = await getMonthlyAverageRatingsService.execute("");
     
         // Assert
         expect(actualResult).to.eql(expectedResult);
     });
 
-    it("execute_WhenNoDataInDB_ShouldReturnStatusCode404", async () => {
+    it("execute_WhenDataInDBAndStoreTypePresent_ShouldReturnStatusCode200AndParticularStoreTypeData", async () => {
+        // Arrange
+        getMonthlyAverageRatingsStub.returns({ data: monthlyAvgRatingsiTunesTestData, status: Operation.Success });
+        const expectedResult = {data: monthlyAvgRatingsiTunesTestData, status: 200};
+
+        // Act
+        const actualResult = await getMonthlyAverageRatingsService.execute(testStoreType);
+    
+        // Assert
+        expect(actualResult).to.eql(expectedResult);
+    });
+
+    it("execute_WhenNoDataInDBAndStoreTypeAbsent_ShouldReturnStatusCode404", async () => {
         // Arrange
         getMonthlyAverageRatingsStub.returns({ data: [], status: Operation.NoDataFound });
         const expectedResult = {data: [], status: 404};
 
         // Act
-        const actualResult = await getMonthlyAverageRatingsService.execute();
+        const actualResult = await getMonthlyAverageRatingsService.execute("");
     
         // Assert
         expect(actualResult).to.eql(expectedResult);
     });
 
-    it("execute_WhenDBError_ShouldReturnStatusCode500", async () => {
+    it("execute_WhenNoDataInDBAndStoreTypePresent_ShouldReturnStatusCode404", async () => {
+        // Arrange
+        getMonthlyAverageRatingsStub.returns({ data: [], status: Operation.NoDataFound });
+        const expectedResult = {data: [], status: 404};
+
+        // Act
+        const actualResult = await getMonthlyAverageRatingsService.execute(testStoreType);
+    
+        // Assert
+        expect(actualResult).to.eql(expectedResult);
+    });
+
+    it("execute_WhenDBErrorAndStoreTypeAbsent_ShouldReturnStatusCode500", async () => {
         // Arrange
         getMonthlyAverageRatingsStub.returns({ status: Operation.Error });
         const expectedResult = {data: undefined, status: 500};
 
         // Act
-        const actualResult = await getMonthlyAverageRatingsService.execute();
+        const actualResult = await getMonthlyAverageRatingsService.execute("");
+    
+        // Assert
+        expect(actualResult).to.eql(expectedResult);
+    });
+
+    it("execute_WhenDBErrorAndStoreTypePresent_ShouldReturnStatusCode500", async () => {
+        // Arrange
+        getMonthlyAverageRatingsStub.returns({ status: Operation.Error });
+        const expectedResult = {data: undefined, status: 500};
+
+        // Act
+        const actualResult = await getMonthlyAverageRatingsService.execute(testStoreType);
     
         // Assert
         expect(actualResult).to.eql(expectedResult);
